69cc8554c3fd230568f05c8fd5e90a0e71644fb5
yogox <yogoyogox@gmail.com>
2021-06-02 16:29:45 +0900

    Initial commit

---
+++ EyeMaskCamera/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeMaskCameraApp.swift
+//  EyeMaskCamera
+//
+//  Created by yogox on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct EyeMaskCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
